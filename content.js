4c1ecb5aed009d3e084eb6ba50c09234f1f7ad94
Hrishika Saxena <hrishika16saxena@gmail.com>
2023-02-20 09:20:54 +0530

    changes

---
+++ content.js
@@ -0,0 +1,11 @@
+(function(){
+    var x = document.querySelectorAll('.artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
+    for(var i = 0; i < x.length; i++){
+        // conn[i].click();
+        console.log('lco')
+    }
+
+    // document.body.appendChild(conn);
+})();
+
+// artdeco-button artdeco-button--2 artdeco-button--secondary ember-view
