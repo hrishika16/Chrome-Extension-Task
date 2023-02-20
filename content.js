f1012eeb9f22333f7e8de8f76a2fe5c958d356c4
Hrishika Saxena <hrishika16saxena@gmail.com>
2023-02-20 17:55:53 +0530

    completed

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
-(function(){
-    var x = document.querySelectorAll('.artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
-    for(var i = 0; i < x.length; i++){
-        // conn[i].click();
-        console.log('lco')
-    }
+(function () {
+	const connectButtons = document.querySelectorAll(".artdeco-button__text");
 
-    // document.body.appendChild(conn);
+	// Iterate over each button and click it after a random wait time
+	connectButtons.forEach((button) => {
+		setTimeout(() => {
+			let temp = button.innerText;
+			if (temp === "Connect") button.click();
+		}, Math.floor(Math.random() * 5000) + 5000);
+	});
 })();
-
-// artdeco-button artdeco-button--2 artdeco-button--secondary ember-view
